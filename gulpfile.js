ba155f9ff1f7859a7e69bdc040b7dc15014888ee
Dementrum <dmitrikolesnikov1976@gmail.com>
2018-09-19 04:11:04 +0900

    Add pug in project

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,10 @@ var gulp          = require('gulp'),
 		autoprefixer  = require('gulp-autoprefixer'),
 		notify        = require("gulp-notify"),
 		rsync         = require('gulp-rsync');
+		pug        		= require('gulp-pug'),
+		htmlBeautify  = require('gulp-html-beautify')
+		plumber       = require('gulp-plumber')
+
 
 gulp.task('browser-sync', function() {
 	browserSync({
@@ -23,7 +27,45 @@ gulp.task('browser-sync', function() {
 		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
 	})
 });
+//Piug in Html  + 
+gulp.task('pug', function() {
+  return gulp.src("app/pug/pages/*.pug")
+	.pipe(pug())
+	.pipe(gulp.dest("app/"))
+	.pipe(browserSync.stream());
+});
 
+gulp.task('htmlBeautify', function() {
+	var options = {
+			indentSize: 2,
+			unformatted: [
+					// https://www.w3.org/TR/html5/dom.html#phrasing-content
+					 'abbr', 'area', 'b', 'bdi', 'bdo', 'br', 'cite',
+					'code', 'data', 'datalist', 'del', 'dfn', 'em', 'embed', 'i', 'ins', 'kbd', 'keygen', 'map', 'mark', 'math', 'meter', 'noscript',
+					'object', 'output', 'progress', 'q', 'ruby', 's', 'samp', 'small',
+					 'strong', 'sub', 'sup', 'template', 'time', 'u', 'var', 'wbr', 'text',
+					'acronym', 'address', 'big', 'dt', 'ins', 'strike', 'tt'
+			]
+
+	};
+gulp.src('app/*.html')
+	.pipe(htmlBeautify(options))
+	.pipe(gulp.dest('app/index'))
+});
+
+gulp.task('pug', function() {
+	return gulp.src("app/pug/pages/*.pug")
+			.pipe(plumber({
+					errorHandler: notify.onError()
+			}))
+			.pipe(pug())
+			.pipe(htmlBeautify())
+			.pipe(gulp.dest("app/"))
+			.pipe(browserSync.stream());
+});
+
+
+//Styles
 gulp.task('styles', function() {
 	// return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
 	return gulp.src('app/sass/main.sass')
@@ -62,9 +104,10 @@ gulp.task('rsync', function() {
 	}))
 });
 
-gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
+gulp.task('watch', ['styles', 'js', 'pug', 'browser-sync'], function() {
 	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
 	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch(['app/pug/**/*.pug'], ['pug']);
 	gulp.watch('app/*.html', browserSync.reload)
 });
 
